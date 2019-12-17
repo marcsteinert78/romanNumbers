79789b4cbc8dc8f9f77d5c47bf168583441b6cb9
Marc Steinert <mcst@ITFSPACE.NET>
2019-12-17 08:31:08 +0100

    minor refactoring

--- src/com/mcst/Main.java
+++ src/com/mcst/Main.java
@@ -1,12 +1,15 @@
 package com.mcst;
 
+import static com.mcst.RomanNumbers.translate;
+
 public class Main {
 
     public static void main(String[] args) {
-       RomanNumbers romanNumbers = new RomanNumbers();
-        System.out.println(romanNumbers.translate("MMXIII"));
-        System.out.println(romanNumbers.translate("XCIX"));
-        System.out.println(romanNumbers.translate("XLII"));
+        System.out.println(translate("MMXIII"));
+        System.out.println(translate("XCIX"));
+        System.out.println(translate("XLII"));
+        System.out.println(translate("XCIII"));
+        System.out.println(translate("MDCCXXXII"));
     }
 
 }
--- src/com/mcst/RomanNumbers.java
+++ src/com/mcst/RomanNumbers.java
@@ -4,21 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RomanNumbers {
-    private Map<Character, Integer> translateMap = new HashMap<>();
+    private static Map<Character, Integer> translateMap = new HashMap<>();
 
-    public RomanNumbers() {
-        this.initMap();
-    }
-
-    public int translate(String romanNumber) {
+    //TODO: check invalidation
+    public static int translate(String romanNumber) {
+        initMap();
         int result = 0;
         while (romanNumber.length() > 1) {
             String pair = romanNumber.substring(0, 2);
-            if (translateMap.get(pair.charAt(0)) < translateMap.get(pair.charAt(1))) {
-                result += (translateMap.get(pair.charAt(1)) - translateMap.get(pair.charAt(0)));
-            } else {
-                result += translateMap.get(pair.charAt(0)) + translateMap.get(pair.charAt(1));
-            }
+            result += (translateMap.get(pair.charAt(1)) +
+                    (translateMap.get(pair.charAt(0)) * pairSmallNumberFirst(pair)));
             romanNumber = romanNumber.replace(pair, "");
         }
         if (romanNumber.length() == 1) {
@@ -27,14 +22,18 @@ public class RomanNumbers {
         return result;
     }
 
-    //TODO: use Map.of....
-    private void initMap() {
-        this.translateMap.put('I', 1);
-        this.translateMap.put('V', 2);
-        this.translateMap.put('X', 10);
-        this.translateMap.put('L', 50);
-        this.translateMap.put('C', 100);
-        this.translateMap.put('D', 500);
-        this.translateMap.put('M', 1000);
+    private static int pairSmallNumberFirst(String pair){
+        return translateMap.get(pair.charAt(0)) < translateMap.get(pair.charAt(1)) ? -1 : 1;
+    }
+
+    //TODO: use ImmutableMap.of....
+    private static void initMap() {
+        translateMap.put('I', 1);
+        translateMap.put('V', 2);
+        translateMap.put('X', 10);
+        translateMap.put('L', 50);
+        translateMap.put('C', 100);
+        translateMap.put('D', 500);
+        translateMap.put('M', 1000);
     }
 }
